54d58a22569d866726b155ee176d9d56f6dc8c6d
Isreal Iyaji <isrealiyaji1@gmail.com>
2025-07-28 12:18:13 +0100

    Updated the blogRoute

--- routes/blogsRoute.js
+++ routes/blogsRoute.js
@@ -10,6 +10,8 @@ const router = express.Router();
 router.post("/", auth, blogsController.createPost);
 router.put("/:id", auth, blogsController.updatePost);
 router.delete("/:id", auth, blogsController.deletePost);
-router.get("/", blogsController.getAllPosts);
+router.get("/", auth, blogsController.getAllPosts);
+router.get("/:id", auth, blogsController.getSinglePost);
+router.get("/my/posts", auth, blogsController.getMyPost);
 
 export default router;
