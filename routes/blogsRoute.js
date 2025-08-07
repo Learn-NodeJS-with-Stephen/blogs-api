0011ceba759381956d2caff966b0344e1775c19c
Isreal Iyaji <isrealiyaji1@gmail.com>
2025-08-07 16:42:25 +0100

    Removed authentication to get all blog post

--- routes/blogsRoute.js
+++ routes/blogsRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/", auth, blogsController.createPost);
 router.put("/:id", auth, blogsController.updatePost);
 router.delete("/:id", auth, blogsController.deletePost);
-router.get("/", auth, blogsController.getAllPost);
+router.get("/", blogsController.getAllPost);
 router.get("/:id", auth, blogsController.getSinglePost);
 router.get("/my/post", auth, blogsController.getMyPost);
 router.get("/:id/similar", auth, blogsController.getSimilarPosts);
