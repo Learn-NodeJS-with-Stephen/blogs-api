a1406b5193ade34b76433d063f00fdc4dd0d9ae1
Isreal Iyaji <isrealiyaji1@gmail.com>
2025-08-20 02:02:29 +0100

    Likes endpoint

--- routes/blogsRoute.js
+++ routes/blogsRoute.js
@@ -2,7 +2,6 @@ import express from "express";
 import usersController from "../controllers/usersController.js";
 import { auth } from "../middleware/auth.js";
 import blogsController from "../controllers/blogsController.js";
-import commentController from "../controllers/commentController.js";
 
 const router = express.Router();
 
@@ -14,5 +13,17 @@ router.get("/:id", auth, blogsController.getSinglePost);
 router.get("/my/post", auth, blogsController.getMyPost);
 router.get("/:id/similar", auth, blogsController.getSimilarPosts);
 router.get("/pagination", blogsController.getAllPostWithPagination);
+
+// Comment
+router.get("/:post_id/comments", auth, blogsController.getCommentsForPost);
+router.post("/posts/:post_id/comments", auth, blogsController.addComment);
+router.put("/comments/:id", auth, blogsController.editComment);
+router.delete("/comments/:id", auth, blogsController.deleteComment);
+
+// Likess
+router.post("/posts/:post_id/like", auth, blogsController.likePost);
+router.delete("/posts/:post_id/like", auth, blogsController.unlikePost);
+router.get("/posts/:post_id/like/:user_id", blogsController.hasUserLikedPost);
+router.get("/posts/:post_id/likes", blogsController.getPostLikes);
 
 export default router;
